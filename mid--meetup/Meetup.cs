e135630c50bc6f56409a98d98e82b900320e0e9d
Davide Monticelli <davide.monticelli@fortech.it>
2024-02-09 11:17:10 +0100

    Aggiunti nuovi esecizi

---
+++ mid--meetup/Meetup.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+public enum Schedule
+{
+    Teenth,
+    First,
+    Second,
+    Third,
+    Fourth,
+    Last
+}
+
+public class Meetup
+{
+    public Meetup(int month, int year)
+    {
+    }
+
+    public DateTime Day(DayOfWeek dayOfWeek, Schedule schedule)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
---
+++ mid--poker/Poker.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public static class Poker
+{
+    public static IEnumerable<string> BestHands(IEnumerable<string> hands)
+    {
+        throw new NotImplementedException();
+    }
+}
