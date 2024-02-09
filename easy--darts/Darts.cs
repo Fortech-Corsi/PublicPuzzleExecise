e4ef9f8b63e7a2579f886dbfb8a46751d8711dd8
Davide Monticelli <davide.monticelli@fortech.it>
2024-02-09 11:43:55 +0100

    aggiunto nuovi progetti easy

---
+++ easy--darts/Darts.cs
@@ -0,0 +1,11 @@
+using System;
+
+public static class Darts
+{
+    public static int Score(double x, double y)
+    {
+       throw new NotImplementedException("You need to implement this function");
+    }
+}
+
+
---
+++ easy--scrabble-score/ScrabbleScore.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ScrabbleScore
+{
+    public static int Score(string input)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
+
