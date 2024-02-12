8d1145cd411902e8cfd371c482b39547d53313f6
Davide Monticelli (Fortech s.r.l.) <davide.monticelli@4ts.it>
2024-02-12 14:39:05 +0100

    comnetato test errato

--- easy--two-fer/TwoFerTests.cs
+++ easy--two-fer/TwoFerTests.cs
@@ -1,22 +1,22 @@
-using Xunit;
+// using Xunit;
 
-public class TwoFerTests
-{
-    [Fact]
-    public void No_name_given()
-    {
-        Assert.Equal("One for you, one for me.", TwoFer.Speak());
-    }
+// public class TwoFerTests
+// {
+//     [Fact]
+//     public void No_name_given()
+//     {
+//         Assert.Equal("One for you, one for me.", TwoFer.Speak());
+//     }
 
-    [Fact]
-    public void A_name_given()
-    {
-        Assert.Equal("One for Alice, one for me.", TwoFer.Speak("Alice"));
-    }
+//     [Fact]
+//     public void A_name_given()
+//     {
+//         Assert.Equal("One for Alice, one for me.", TwoFer.Speak("Alice"));
+//     }
 
-    [Fact]
-    public void Another_name_given()
-    {
-        Assert.Equal("One for Bob, one for me.", TwoFer.Speak("Bob"));
-    }
-}
+//     [Fact]
+//     public void Another_name_given()
+//     {
+//         Assert.Equal("One for Bob, one for me.", TwoFer.Speak("Bob"));
+//     }
+// }
